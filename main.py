902ecfa4067ec97609be047417a73a286bd8ea20
Dorthe Luebbert <dorthe@luebbert.net>
2019-05-10 06:53:46 +0200

    initial

---
+++ main.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+from lib import calc
+
+# Read .env file 
+load_dotenv(verbose=True)
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+# does it work?
+print("Hallo ",os.getenv('USERNAME'))
+
+# awesome calculation!
+print("3 plus 5 ist {}!" .format(calc.sum(3,5)))
+
