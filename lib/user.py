3dfa31cdb6728b634716bcda730652cb576dc889
Dorthe Luebbert <dorthe@luebbert.net>
2019-05-12 18:08:32 +0200

    class added

---
+++ lib/user.py
@@ -0,0 +1,7 @@
+class User:
+
+    def __init__(self, firstname):
+        self.firstname = firstname
+
+    def print_info(self):
+        print("Das ist also ", self.firstname)
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 from pathlib import Path
 import os
 from lib import calc
+from lib.user import User
+
 
 # Read .env file 
 load_dotenv(verbose=True)
@@ -11,6 +13,13 @@ load_dotenv(dotenv_path=env_path)
 # does it work?
 print("Hallo lieber ",os.getenv('USERNAME'))
 
-# awesome calculation!
+# awesome calculation! 
 print("3 plus 5 ist {}!" .format(calc.sum(3,5)))
+
+# does it also find the Python class?
+u = User("Gustav")
+u.print_info()
+
+
+
 
