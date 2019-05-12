e8a363ce62532fb7893a806bff52dc9623891b8b
Dorthe Luebbert <dorthe@luebbert.net>
2019-05-12 13:16:22 +0200

    playing around with visual code git integration

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 # does it work?
-print("Hallo ",os.getenv('USERNAME'))
+print("Hallo lieber ",os.getenv('USERNAME'))
 
 # awesome calculation!
 print("3 plus 5 ist {}!" .format(calc.sum(3,5)))
